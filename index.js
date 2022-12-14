08368f09e432445f9c89d6d49be823f480a6c791
Syed Anas <59249245+Syedanas-ctrl@users.noreply.github.com>
2022-12-14 23:16:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const app = express()
 const port = 4000
 const cors = require("cors")
 
-// npm init
-// npm i express cors nodemon
-// they add a handy req.body object to our req,
-// containing a Javascript
-//  object representing the payload sent with the request
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
@@ -21,10 +15,7 @@ app.get("/home", cors(), async (req, res) => {
 	res.send(data)
 })
 
-// app.post("/post_name", async (req, res) => {
-// 	let { name } = req.body
-// 	console.log(name)
-// })
+
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`)
 })
